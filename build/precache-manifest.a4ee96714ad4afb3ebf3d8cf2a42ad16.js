56373f53d83f34ae1bd155f447d2b464ea683a3a
benjaminDom1 <benjamin.j.dominguez.o@gmail.com>
2021-05-26 19:24:23 -0700

    comit build

---
+++ build/precache-manifest.a4ee96714ad4afb3ebf3d8cf2a42ad16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d54e3ca51a5ad5e050c",
+    "url": "./static/css/main.a7f7aba2.chunk.css"
+  },
+  {
+    "revision": "9d54e3ca51a5ad5e050c",
+    "url": "./static/js/main.9d54e3ca.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "./static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "c04fe0bd6249d16ee69c8de8fddbdf83",
+    "url": "./index.html"
+  }
+];
